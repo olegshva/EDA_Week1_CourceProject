e40d988a189092999562d3d6147f83c4d947c748
Oleg Shvaikovsky <oleg@shvaikovsky.com>
2015-01-11 20:10:23 +0200

    plot 1 and 2

--- Plot2.R
+++ Plot2.R
@@ -1,13 +1,13 @@
 ## (c) Oleg Shvaikovsky
 ##     Exploratory Data Analysis
-##     week 1  :::: PLOT 1
+##     week 1 ::: PLOT 2 
 ## ->  SANDBOX, week 1
 
 setwd("/Users/olegshvaikovskypro/R-Coursera/ExploratoryDataAnalysis/Week1")
 
 ## Reading all the data
 FullData <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
-                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+                     nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 FullData$Date <- as.Date(FullData$Date, format="%d/%m/%Y")
 
 ## Subsetting the data from FullData
@@ -16,11 +16,13 @@ MyData <- subset(FullData, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 ## Cleaning the op.memory
 rm(FullData)
 
-## Creating the histogram
-hist(MyData$Global_active_power, main="Global Active Power",
-xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+DT <- paste(as.Date(MyData$Date), MyData$Time)
+MyData$Datetime <- as.POSIXct(DT)
+
+## Creating the plot
+plot(MyData$Global_active_power ~ MyData$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
 
 ## Saving to file
-dev.copy(png, file="plot1.png", height=480, width=480)
+dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
-
